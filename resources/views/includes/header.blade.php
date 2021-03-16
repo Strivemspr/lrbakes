25f43bc591b8e3d1924db75c9119cf5469d4e2fb
unknown <marlonspr@hotmail.com>
2021-03-17 10:28:53 +1300

    home page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,17 +1,28 @@
-<div class="container flex">
-    <ul class="flex w-1/3">
-        <li><a href="/home">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/gallery">Gallery</a></li>
-    </ul>
-
-    <div class="w-1/3">
-        <h1>LR BAKES.</h1>
+<div class="bg-white">
+    <div class="lg:container flex md:flex-row flex-col md:p-3 items-center">
+        <ul class="md:flex hidden md:w-1/3 w-full text-center md:order-1 order-2">
+            <li class="link-default"><a href="/home">Home</a></li>
+            <li class="link-default"><a href="/about">About</a></li>
+            <li class="link-default"><a href="/gallery">Gallery</a></li>
+        </ul>
+    
+        <div class="md:w-1/3 md:order-2 order-1 flex w-full justify-between items-center md:p-0 p-2">
+            <div class="md:block md:mx-auto flex items-center">
+                <img class="md:mx-auto md:h-20 h-15 mr-3" src="{{URL('/images/logo.svg')}}">
+                <h1 class="text-center text-3xl italic font-black text-secondary-DEFAULT">LR BAKES.</h1>
+            </div>
+            <div class="md:hidden">
+                <i class="mr-2 icon fab fa-facebook-square fa-2x"></i>
+                <i class="mr-2 icon fab fa-instagram fa-2x"></i>
+                <i class="fas fa-bars fa-2x text-black-DEFAULT cursor-pointer"></i>
+            </div>
+        </div>
+        
+    
+        <ul class="order-3 md:w-1/3 w-full hidden md:flex-no-wrap flex-wrap md:justify-end justify-center items-center">
+            <li class="mr-6 md:block hidden icon"><a class="block" href="#"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+            <li class="mr-6 md:block hidden icon"><a class="block" href="#"><i class="fab fa-instagram fa-2x"></i></a></li>
+            <li class="md:w-auto w-full btn btn-small"><a class="text-center block" href="/contact">Get a Quote</a></li>
+        </ul>
     </div>
-
-    <ul class="w-1/3">
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="/contact">Get a Quote</a></li>
-    </ul>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,33 @@ module.exports = {
     },
     container: {
       'center': true,
-    }
+    },
+    // colors: theme => ({
+    //   ...theme('colors'),
+    //   'primary': '#8F6ADD',
+    //   'secondary': '#ffed4a',
+    //   'danger': '#e3342f',
+    //  }),
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      primary: {
+        DEFAULT: '#8F6ADD', // purple
+        light: '#9D77EE', 
+      }, 
+      secondary: {
+        DEFAULT: '#FF6060',   // red
+      },
+      grey: {
+        dark: '#EAEAEA',
+        DEFAULT: '#EFEFEF',
+        light: '#F4F4F4',
+      },
+      black: {
+        DEFAULT: '#3C3C3C',
+        light: '#ACACAC'
+      }
+    },
   },
   variants: {},
   plugins: [
